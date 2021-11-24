15039aa64e0074456893da561c4eb995e36dd6a6
Arpad Rozsas <arpad.rozsas@tno.nl>
2021-11-24 21:55:33 +0100

    fix(?): set env variable for headless mode

--- warmup.jl
+++ warmup.jl
@@ -1,6 +1,9 @@
 using Plots
 using Distributions
 
+# https://discourse.julialang.org/t/generation-of-documentation-fails-qt-qpa-xcb-could-not-connect-to-display/60988/2?u=rozsasarpi
+ENV["GKSwstype"] = "100"
+
 mu = 3.14;
 sigma = 1.0;
 x = [mu - 5 * sigma:0.1:mu + 5 * sigma;];
