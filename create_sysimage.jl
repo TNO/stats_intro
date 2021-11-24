93d133996394f02fe849e67c24a7e501f85afc94
Arpad Rozsas <arpad.rozsas@tno.nl>
2021-11-24 20:23:11 +0100

    fix: provide `sysimage_path` argument to `create_sysimage`
    - based on https://github.com/JuliaLang/PackageCompiler.jl/commit/e4716cca918eea77b9703465892911b09b2edd2b

--- create_sysimage.jl
+++ create_sysimage.jl
@@ -1,5 +1,11 @@
+using Libdl
 using PackageCompiler
+
+tmp = mktempdir()
+sysimage_path = joinpath(tmp, string("sys.", Libdl.dlext))
+
 create_sysimage([:Pluto, :PlutoUI, :Distributions, :Plots];
+                sysimage_path = sysimage_path,
                 precompile_execution_file = "warmup.jl",
                 replace_default = true,
                 cpu_target = PackageCompiler.default_app_cpu_target())
